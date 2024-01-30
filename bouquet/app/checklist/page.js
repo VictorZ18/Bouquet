70d0bb91f41cc8771f66fef4df0eb03360f04ea3
VictorZ18 <r0879539@student.thomasmore.be>
2024-01-30 11:22:17 +0100

    Created basic pages for functionalities with routes to each via buttons

---
+++ bouquet/app/checklist/page.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import styles from '../page.module.css';
+
+export default function Suppliers() {
+    return (
+        <div className={styles.header}>
+        <h1 className={styles.title}>This is checklist</h1>
+        <button className={styles.button}><Link href="/">Home</Link></button>
+        <button className={styles.button}><Link href="/suppliers">Suppliers</Link></button>
+        <button className={styles.button}><Link href="/guests">Guests</Link></button>
+        <button className={styles.button}><Link href="/program">Program</Link></button>
+        </div>
+    );
+}
